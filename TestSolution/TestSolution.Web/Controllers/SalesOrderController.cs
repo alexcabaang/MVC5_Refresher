b7bf7e001f06d2260cc4c4ddda54946581c24d7a
Alex <alexcabaang@gmail.com>
2017-07-16 15:13:16 +0800

    Added Controller Scaffolding

--- TestSolution/TestSolution.Web/Controllers/SalesOrderController.cs
+++ TestSolution/TestSolution.Web/Controllers/SalesOrderController.cs
@@ -1,13 +1,129 @@
-﻿using System.Web.Mvc;
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using TestSolution.DataLayer;
+using TestSolution.Model;
 
 namespace TestSolution.Web.Controllers
 {
     public class SalesOrderController : Controller
     {
+        private readonly SalesContext db;
+
+        public SalesOrderController()
+        {
+            db = new SalesContext();
+        }
+
         // GET: SalesOrder
         public ActionResult Index()
         {
+            return View(db.SalesOrders.ToList());
+        }
+
+        // GET: SalesOrder/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SalesOrder salesOrder = db.SalesOrders.Find(id);
+            if (salesOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salesOrder);
+        }
+
+        // GET: SalesOrder/Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        // POST: SalesOrder/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SalesOrderId,CustomerName,PoNumber,OrderDate")] SalesOrder salesOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SalesOrders.Add(salesOrder);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(salesOrder);
+        }
+
+        // GET: SalesOrder/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SalesOrder salesOrder = db.SalesOrders.Find(id);
+            if (salesOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salesOrder);
+        }
+
+        // POST: SalesOrder/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SalesOrderId,CustomerName,PoNumber,OrderDate")] SalesOrder salesOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(salesOrder).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(salesOrder);
+        }
+
+        // GET: SalesOrder/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SalesOrder salesOrder = db.SalesOrders.Find(id);
+            if (salesOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salesOrder);
+        }
+
+        // POST: SalesOrder/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SalesOrder salesOrder = db.SalesOrders.Find(id);
+            db.SalesOrders.Remove(salesOrder);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
